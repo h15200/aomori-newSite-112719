2b1e4a1145eb1672d27053c08930404f5614dd6f
h15200 <45977354+h15200@users.noreply.github.com>
2019-11-27 09:29:29 -0800

    set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = { 
+  "extends": [
+    "airbnb-base",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures":{
+      "jsx": true
+    }
+  },
+  "rules": {
+    "arrow-parens": [
+      "error", "as-needed"
+    ],
+    "comma-dangle": 1,
+    "no-console": 1,
+    "quotes": 0,
+    "no-multiple-empty-lines": 0,
+    "semi": 0,
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "local",
+        "args": "none"
+      }
+    ]
+  },
+  "env": {
+    "browser": true
+  },
+}
