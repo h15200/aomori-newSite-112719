6966fbdbd102e07c30a82b8c5607eb0434cf3e60
h15200 <45977354+h15200@users.noreply.github.com>
2019-12-03 13:15:17 -0800

    added mailto and designed email button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React from "react"
 
-import Layout from '../components/layout'
-import contactStyles from '../styles/contact.module.scss'
+import Layout from "../components/layout"
+import contactStyles from "../styles/contact.module.scss"
 
 const ContactPage = () => (
   <Layout>
     <div className={contactStyles.header}>
       <h2>Contact me at</h2>
-      <p>leadingToneRecords@gmail.com</p>
+      <a
+        href="mailto:leadingtoneRecords@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LeadingtoneRecords@gmail.com
+      </a>
     </div>
     <div className={contactStyles.container}>
-      <div id="studio" className={contactStyles.studioContainer}>
-        <h2>Studio Work</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit minima
-          iste architecto dolorem eos hic qui ipsa, sapiente ipsam, sunt nulla,
-          eaque blanditiis. Provident numquam hic, earum atque quae quis!
-        </p>
-      </div>
-      <div id="education" className={contactStyles.educationContainer}>
-        <h2>education</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          repudiandae ullam odit molestiae nemo id, inventore eligendi nulla
-          minus? Architecto, maiores assumenda? Tempora ad commodi obcaecati,
-          nobis ex excepturi quis.
-        </p>
-      </div>
+      <h2 id="studio">Studio Work</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit minima
+        iste architecto dolorem eos hic qui ipsa, sapiente ipsam, sunt nulla,
+        eaque blanditiis. Provident numquam hic, earum atque quae quis!
+      </p>
+
+      <h2 id="education">education</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+        repudiandae ullam odit molestiae nemo id, inventore eligendi nulla
+        minus? Architecto, maiores assumenda? Tempora ad commodi obcaecati,
+        nobis ex excepturi quis.
+      </p>
     </div>
   </Layout>
 )
