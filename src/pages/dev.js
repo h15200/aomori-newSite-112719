7c2744a076660892177d5fd73a9cc7e7df1adf06
h15200 <45977354+h15200@users.noreply.github.com>
2020-02-06 15:11:36 -0800

    added hover and active states on nav and footer for better ux

---
+++ src/pages/dev.js
@@ -0,0 +1,15 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+import devStyles from '../styles/dev.module.scss'
+
+const Dev = () => {
+  return (
+    <div className={devStyles.devPage}>
+      <div className={devStyles.container}>
+        hi
+      </div>
+    </div>
+  )
+}
+
+export default Dev
