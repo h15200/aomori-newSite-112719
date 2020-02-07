9c5a9ff46417a1d7c4c5f6589a7cb3a09e3e3821
h15200 <45977354+h15200@users.noreply.github.com>
2020-02-06 16:22:06 -0800

    start dev page layout

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -1,13 +1,38 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
-import devStyles from '../styles/dev.module.scss'
+import React, { Fragment } from "react"
+import devStyles from "../styles/dev.module.scss"
 
 const Dev = () => {
   return (
     <div className={devStyles.devPage}>
-      <div className={devStyles.container}>
-        hi
-      </div>
+      <header className={devStyles.header}>
+        <div className={`${devStyles.container} ${devStyles.container___header}`}>
+          Header
+        </div>
+      </header>
+
+      <section className={devStyles.skills}>
+        <div
+          className={`${devStyles.container} ${devStyles.container___skills}`}
+        >
+          Skills
+        </div>
+      </section>
+
+      <section className={devStyles.projects}>
+        <div
+          className={`${devStyles.container} ${devStyles.container___projects}`}
+        >
+          Projects
+        </div>
+      </section>
+      <footer className={devStyles.contact}>
+        <div
+          className={`${devStyles.container} ${devStyles.container___footer}`}
+        >
+          Footer
+        </div>
+      </footer>
     </div>
   )
 }
