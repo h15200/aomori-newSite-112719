e119428d4b2bd290ac5862522ec50de10b1fe518
h15200 <45977354+h15200@users.noreply.github.com>
2020-02-07 20:02:38 -0800

    change dev page meta data

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -1,39 +1,48 @@
 /* eslint-disable arrow-body-style */
-import React from "react"
+import React, { Fragment } from "react"
+import { Helmet } from 'react-helmet'
 import devStyles from "../styles/dev.module.scss"
+
 
 const Dev = () => {
   return (
-    <div className={devStyles.devPage}>
-      <header className={devStyles.header}>
-        <div className={`${devStyles.container} ${devStyles.container___header}`}>
-          Header
-        </div>
-      </header>
+    <Fragment>
+      <Helmet>
+        <title>Webdev & Design</title>
+      <meta charSet="utf-8" name="description" content="Hideaki Aomori - Web devloper and designer"/>
+      </Helmet>
+      <div className={devStyles.devPage}>
 
-      <section className={devStyles.skills}>
-        <div
-          className={`${devStyles.container} ${devStyles.container___skills}`}
-        >
-          Skills
-        </div>
-      </section>
+        <header className={devStyles.header}>
+          <div className={`${devStyles.container} ${devStyles.container___header}`}>
+            Header
+          </div>
+        </header>
 
-      <section className={devStyles.projects}>
-        <div
-          className={`${devStyles.container} ${devStyles.container___projects}`}
-        >
-          Projects
-        </div>
-      </section>
-      <footer className={devStyles.contact}>
-        <div
-          className={`${devStyles.container} ${devStyles.container___footer}`}
-        >
-          Footer
-        </div>
-      </footer>
-    </div>
+        <section className={devStyles.skills}>
+          <div
+            className={`${devStyles.container} ${devStyles.container___skills}`}
+          >
+            Skills
+          </div>
+        </section>
+
+        <section className={devStyles.projects}>
+          <div
+            className={`${devStyles.container} ${devStyles.container___projects}`}
+          >
+            Projects
+          </div>
+        </section>
+        <footer className={devStyles.contact}>
+          <div
+            className={`${devStyles.container} ${devStyles.container___footer}`}
+          >
+            Footer
+          </div>
+        </footer>
+  </div>
+</Fragment>
   )
 }
 
