40426c23c70370a1afe1922ced41068364483c6c
h15200 <45977354+h15200@users.noreply.github.com>
2020-02-12 19:38:48 -0800

    add logos to all skills

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -7,6 +7,16 @@ import Img from "gatsby-image"
 import faviconDev from "../image/faviconDev.ico"
 import devStyles from "../styles/dev.module.scss"
 
+import Html from '../svg/html5.svg'
+import Css from '../svg/css3.svg'
+import Sass from '../svg/sass.svg'
+import Javascript from '../svg/javascript.svg'
+import ReactIcon from '../svg/reactIcon.svg'
+import GraphqlIcon from '../svg/graphql.svg'
+import GatsbyIcon from '../svg/gatsby.svg'
+import Git from '../svg/git.svg'
+import Figma from '../svg/figma.svg'
+
 import Code from '../svg/code.svg'
 import Email from '../svg/email.svg'
 import Github from '../svg/github.svg'
@@ -91,15 +101,18 @@ const Dev = () => {
             <div className={`${devStyles.skills} `}>
               <div className={devStyles.skills_header_2}>Skills:</div>
               <ul className={devStyles.skills_list}>
-                <li className={devStyles.skills_item}>html</li>
-                <li className={devStyles.skills_item}>css</li>
-                <li className={devStyles.skills_item}>sass</li>
-                <li className={devStyles.skills_item}>javascript</li>
-                <li className={devStyles.skills_item}>reac</li>
-                <li className={devStyles.skills_item}>graphql</li>
-                <li className={devStyles.skills_item}>gatsby</li>
-                <li className={devStyles.skills_item}>git</li>
-                <li className={devStyles.skills_item}>figma</li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Html className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>html</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Css className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>css</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Sass className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>sass</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Javascript className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>javascript</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><ReactIcon className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>react</figcaption></figure></li>
+               
+               
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><GraphqlIcon className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>graphql</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><GatsbyIcon className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>gatsby</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Git className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>Git</figcaption></figure></li>
+                <li className={devStyles.skills_item}><figure className={devStyles.skills_figure}><Figma className={devStyles.skills_icon}/><figcaption className={devStyles.skills_caption}>Figma</figcaption></figure></li>
+
               </ul>
             </div>
           </div>
@@ -111,8 +124,10 @@ const Dev = () => {
           >
             <h2 className={devStyles.projects_header_2}>Recent Projects</h2>
             <div className={`${devStyles.card}`}>
+            <Img fluid={data.steve_fav.childImageSharp.fluid} className={devStyles.card_icon} />
             <Code />
             </div>
+
           </div>
         </section>
         <footer className={devStyles.contact} id="contact">
