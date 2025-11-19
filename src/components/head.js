37f0ec14e093205768294331b9ebd377dfb337cd
h15200 <45977354+h15200@users.noreply.github.com>
2025-11-18 19:29:49 -0800

    add: pinterest account claim

--- src/components/head.js
+++ src/components/head.js
@@ -1,7 +1,7 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import { Helmet } from "react-helmet"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
 
 const Head = ({ title }) => {
   const data = useStaticQuery(graphql`
@@ -12,17 +12,23 @@ const Head = ({ title }) => {
         }
       }
     }
-  `)
+  `);
   return (
     <Helmet>
       <title>{`${title} | ${data.site.siteMetadata.title}`}</title>
-      <meta charSet="utf-8" name="description" content="Information about Hideaki Aomori, a world-class woodwind musician who plays clarinet, saxophone, and flute"/>
+      <meta
+        charSet="utf-8"
+        name="description"
+        content="Information about Hideaki Aomori, a world-class woodwind musician who plays clarinet, saxophone, and flute"
+      />
+      {/* pinterest account claming */}
+      <meta name="p:domain_verify" content="e2ea63c5d376890df698cd03f50e02be" />
     </Helmet>
-  )
-}
+  );
+};
 
 Head.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
-export default Head
+export default Head;
